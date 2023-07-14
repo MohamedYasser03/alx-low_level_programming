0c02715fd549a020078cb7e78b7f5bd595016f42
sadat <my841319@gmail.com>
2023-07-14 08:48:53 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: print a qoute using write function
- *	ssize_t write(int fd. const void *buf. size_t const);
+ *	ssize_t write(int fd, const void *buf, size_t const);
  *
  * Return: 1 (error).
 */
@@ -12,6 +12,6 @@
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. quo. 59);
+	write(1, quo, 59);
 	return (1);
 }
