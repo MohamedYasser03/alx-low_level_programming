0a9cf22637b4baadd2345a63ac64f3d5c582911c
sadat <my841319@gmail.com>
2023-07-20 10:18:23 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checks if character is a letter
+ *
+ * @a: checks input of function
+ * @b: second
+ *
+ * Return: 1 if true, 0 if false
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
