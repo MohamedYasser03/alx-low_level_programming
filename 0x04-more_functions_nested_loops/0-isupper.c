7d0bfe64b5a727552c443cf561d14f60661a8718
sadat <my841319@gmail.com>
2023-07-20 10:13:10 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is a letter
+ *
+ * @c: checks input of function
+ *
+ * Return: 1 if true, 0 if false
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
