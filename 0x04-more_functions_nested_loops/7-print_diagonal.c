0e9dd47cab1d4c2ed87be6e1ed6db7f42826dc20
sadat <my841319@gmail.com>
2023-07-20 10:45:00 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - checks if character is a letter
+ *
+ * @n: checks input of function
+ *
+ * Return: 1 if true, 0 if false
+*/
+
+void print_diagonal(int n)
+{
+	int p, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 1; p <= n; p++)
+		{
+			for (s = 1; s <= p; s++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
