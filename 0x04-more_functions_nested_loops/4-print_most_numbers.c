8fd73f7536b9743ae2931bf5dcc2016e3885783b
sadat <my841319@gmail.com>
2023-07-20 10:30:48 -0700

    My first commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks if character is a letter
+ * print_most_numbers - prints 0 - 9 apart
+ *			from 2 and 4 you can
+ *			only use _putchar twice
  *
- * Return: 1 if true, 0 if false
+ * Return: Always 0 (Success)
 */
 
 void print_most_numbers(void)
@@ -12,7 +14,7 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 && num == 4)
+		if (num == 2 || num == 4)
 			continue;
 		_putchar(num + 48);
 	}
