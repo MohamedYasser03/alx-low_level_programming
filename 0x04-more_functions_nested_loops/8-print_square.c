534634c2676835b38257470ae386521a2a30a387
sadat <my841319@gmail.com>
2023-07-20 11:11:21 -0700

    My first commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,10 +4,19 @@
  * print_square - checks if character is a letter
  *
  * @size: size
+ *
  * Return: 1 if true, 0 if false
 */
 
 void print_square(int size)
 {
-	
+	int row, col;
+
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
