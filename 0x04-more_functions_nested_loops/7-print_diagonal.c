bcf7b694b6a431daeaef3c318ee3f2910e395bbc
sadat <my841319@gmail.com>
2023-07-20 10:50:33 -0700

    My first commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 		{
 			for (s = 1; s <= p; s++)
 				_putchar(' ');
-			_putchar(92);
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_square - checks if character is a letter
+ *
+ * @size: size
+ * Return: 1 if true, 0 if false
+*/
+
+void print_square(int size)
+{
+	
+}
