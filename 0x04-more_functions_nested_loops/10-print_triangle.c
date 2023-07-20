38ea7f1a78e0f56e852cb3f24b60fe23548a4034
sadat <my841319@gmail.com>
2023-07-20 11:58:04 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - checks if character is a letter
+ *
+ * @size: size
+ * Return: 1 if true, 0 if false
+*/
+
+void print_triangle(int size)
+{
+	int h, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((h + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
